ba9984f17cbe8886ee7d0f9d969f4ddbbc03233a
abhi <traningsanyal06@gmail.com>
2019-02-27 23:22:28 -0500

    newly add

--- new.java
+++ new.java
@@ -1,3 +1,5 @@
 Hi This is my third file which i want to make live on the internet. hence
 I want every one to please cooperate with us. Thanks you very much.
 I also want to thanks to all my new mates on the new project.
+
+Hi i want to add this new stuff i
